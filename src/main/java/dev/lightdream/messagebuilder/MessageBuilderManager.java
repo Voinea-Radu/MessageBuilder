8bcd1d13eab662c6d686cc5208b9b6507b4a4dc2
L1ghtDream <radu.voinea.mihai@gmail.com>
2022-03-18 03:58:49 +0200

    Added version print-out

--- src/main/java/dev/lightdream/messagebuilder/MessageBuilderManager.java
+++ src/main/java/dev/lightdream/messagebuilder/MessageBuilderManager.java
@@ -3,9 +3,16 @@ package dev.lightdream.messagebuilder;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonSerializer;
 import dev.lightdream.filemanager.FileManager;
+import dev.lightdream.filemanager.FileManagerMain;
 
 @SuppressWarnings("unused")
 public class MessageBuilderManager {
+
+    @SuppressWarnings("unused")
+    static String getVersion() {
+        return "MessageBuilder " + MessageBuilder.class.getPackage().getImplementationVersion() +
+                "    -> " + FileManagerMain.getVersion();
+    }
 
     public static void registerFileManagerModule(FileManager fileManager) {
         JsonSerializer<MessageBuilder> serializer = new MessageBuilderSerializer();
